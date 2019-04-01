831e91bd37fb553d62c3f4ff1fb1a8a9ff526c54
Viet Nguyen <vietjul@gmail.com>
2019-04-01 10:05:06 -0400

    feat: components: NotFound, pages

--- user-auth-amplify/src/components/Routes.js
+++ user-auth-amplify/src/components/Routes.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from "./Home";
-import Login from './Login';
+
+import {Home, Login, NotFound} from './pages';
 
 export default () =>
     <Router>
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/login" exact component={Login} />
+            <Route component={NotFound} />
         </Switch>
     </Router>
   
---
+++ user-auth-amplify/src/components/pages/index.js
@@ -0,0 +1,5 @@
+import {default as Home} from '../Home';
+import {default as Login} from '../Login';
+import {default as NotFound} from '../NotFound';
+
+export {Home, Login, NotFound};
