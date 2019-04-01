7ca20da3536ef0c760dbc11f37a3bd25bdc3aee7
Viet Nguyen <vietjul@gmail.com>
2019-04-01 09:10:37 -0400

    feat: update Home.js and Login.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,8 @@ export default class Home extends Component {
     return (
       <div className="Home">
         <div className="lander">
-          <h1>Scratch</h1>
-          <p>A simple note taking app</p>
+          <h1>Test App</h1>
+          <p>User Auth with AWS Amplify and AWS Cognito</p>
         </div>
       </div>
     );
