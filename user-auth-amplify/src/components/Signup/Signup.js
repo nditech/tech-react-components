4b26a54831b6d8c9aeb9ff99b5a7f2a0f0c6d4f1
Viet Nguyen <vietjul@gmail.com>
2019-04-02 10:05:30 -0400

    feat: signup new users successfully

--- user-auth-amplify/src/components/Signup/Signup.js
+++ user-auth-amplify/src/components/Signup/Signup.js
@@ -6,6 +6,8 @@ import {
   Input,
   FormText
 } from "reactstrap";
+import { Auth } from "aws-amplify";
+
 import LoaderBtn from "../LoaderBtn";
 import "./Signup.css";
 
@@ -43,19 +45,41 @@ export default class Signup extends Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+  
     this.setState({ isLoading: true });
-
-    this.setState({ newUser: "test" });
-
+  
+    try {
+      const newUser = await Auth.signUp({
+        username: this.state.email,
+        password: this.state.password
+      });
+      this.setState({
+        newUser
+      });
+    } catch (e) {
+      alert(e.message);
+    }
+  
     this.setState({ isLoading: false });
   }
-
+  
   handleConfirmationSubmit = async event => {
     event.preventDefault();
-
+  
     this.setState({ isLoading: true });
+  
+    try {
+      await Auth.confirmSignUp(this.state.email, this.state.confirmationCode);
+      await Auth.signIn(this.state.email, this.state.password);
+  
+      this.props.userHasAuthenticated(true);
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      this.setState({ isLoading: false });
+    }
   }
+  
 
   renderConfirmationForm() {
     return (
